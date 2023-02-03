333f03ca7f365170e29e33e5344e52e51a255e7e
Mohamad Hussein <hammoudi5672@gmail.com>
2023-02-03 13:59:27 +0100

    fuckinglorteopgave

--- produkt.js
+++ produkt.js
@@ -1,4 +1,4 @@
-const url = "https://kea-alt-del.dk/t7/api/products/1163";
+const url = "https://kea-alt-del.dk/t7/api/products/1573";
 const imagePath = "https://kea-alt-del.dk/t7/images/webp/640/1573.webp";
 function hentData() {
   fetch(url)
@@ -8,6 +8,8 @@ function hentData() {
 function visProdukt(produkt) {
   console.log(produkt);
   document.querySelector("#model").textContent = produkt.productdisplayname;
+  document.querySelector("#gender").textContent = produkt.gender;
+  document.querySelector("#pris").textContent = produkt.price;
   document.querySelector("img").src = imagePath;
 }
 
